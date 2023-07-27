8d5a6b820d9b0e6956ff0913af82188bb558e02b
RafaelAdrianMejia <tucsonrafa@outlook.com>
2023-07-27 14:01:18 -0700

    initial commit

---
+++ Variables_Excercise/Variables_Excercise/Program.cs
@@ -0,0 +1,16 @@
+﻿// Declaring and initialize variables 
+using System;
+
+string name = "Bob";
+int age = 25;
+char middleInitial = 'M';
+bool isOver18 = true;
+double currentTemp = 114;
+decimal currentPrice = 125.5m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {currentTemp}");
+Console.WriteLine($"Current Price: {currentPrice}");
